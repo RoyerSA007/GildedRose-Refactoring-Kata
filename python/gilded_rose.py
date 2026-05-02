191a9d7cd33b2c35e9484361669d44463cc74ff0
RoyerSA007 <rogeasa@outlook.es>
2026-05-02 17:33:34 -0600

    Correccion de constantes, Creacion de modulos

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -1,39 +1,65 @@
 # -*- coding: utf-8 -*-
 
 class GildedRose(object):
+    # --- Constantes para evitar "Magic Strings" ---
+    AGED_BRIE = "Aged Brie"
+    BACKSTAGE_PASSES = "Backstage passes to a TAFKAL80ETC concert"
+    SULFURAS = "Sulfuras, Hand of Ragnaros"
 
     def __init__(self, items):
         self.items = items
 
     def update_quality(self):
         for item in self.items:
-            if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
-                if item.quality > 0:
-                    if item.name != "Sulfuras, Hand of Ragnaros":
-                        item.quality = item.quality - 1
-            else:
-                if item.quality < 50:
-                    item.quality = item.quality + 1
-                    if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                        if item.sell_in < 11:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-                        if item.sell_in < 6:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-            if item.name != "Sulfuras, Hand of Ragnaros":
-                item.sell_in = item.sell_in - 1
-            if item.sell_in < 0:
-                if item.name != "Aged Brie":
-                    if item.name != "Backstage passes to a TAFKAL80ETC concert":
-                        if item.quality > 0:
-                            if item.name != "Sulfuras, Hand of Ragnaros":
-                                item.quality = item.quality - 1
-                    else:
-                        item.quality = item.quality - item.quality
-                else:
-                    if item.quality < 50:
-                        item.quality = item.quality + 1
+            self._update_item_status(item)
+
+    def _update_item_status(self, item):
+        """Orquesta la actualización de un ítem según su nombre."""
+        if item.name == self.SULFURAS:
+            return  # Sulfuras no cambia nunca
+
+        if item.name == self.AGED_BRIE:
+            self._update_aged_brie(item)
+        elif item.name == self.BACKSTAGE_PASSES:
+            self._update_backstage_pass(item)
+        else:
+            self._update_normal_item(item)
+
+        # Reducción del tiempo de venta para todos excepto Sulfuras
+        item.sell_in -= 1
+
+        # Lógica post-vencimiento
+        if item.sell_in < 0:
+            self._handle_expired_item(item)
+
+    # --- Métodos Privados de Lógica Específica ---
+
+    def _update_aged_brie(self, item):
+        if item.quality < 50:
+            item.quality += 1
+
+    def _update_backstage_pass(self, item):
+        if item.quality < 50:
+            item.quality += 1
+            if item.sell_in < 11 and item.quality < 50:
+                item.quality += 1
+            if item.sell_in < 6 and item.quality < 50:
+                item.quality += 1
+
+    def _update_normal_item(self, item):
+        if item.quality > 0:
+            item.quality -= 1
+
+    def _handle_expired_item(self, item):
+        """Define qué pasa cuando sell_in < 0."""
+        if item.name == self.AGED_BRIE:
+            if item.quality < 50:
+                item.quality += 1
+        elif item.name == self.BACKSTAGE_PASSES:
+            item.quality = 0
+        else:
+            if item.quality > 0:
+                item.quality -= 1
 
 
 class Item:
